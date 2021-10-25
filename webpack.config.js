e77d85af630935aade05757f2153b5cc222fe14a
Nicolas Ricardi <nickricardi00@gmail.com>
2021-10-26 00:56:40 +0200

    Mobile view

--- webpack.config.js
+++ webpack.config.js
@@ -10,31 +10,8 @@ module.exports = {
     devtool: 'inline-source-map',
     plugins:[
         new HtmlWebpackPlugin({
-            title: 'Pollution App',
             inject: 'body',
             template: 'index.html',
-            meta:{
-                viewport: 'width=device-width, initial-scale=1',
-                title: 'pollution app',
-                description: 'description pollution app',
-                keywords: 'Pollution, Air Quality App',
-                robots: 'follow, index, max-snippet:-1, max-video-preview:-1, max-image-preview:large',
-                'twitter:card': 'summary',
-                'twitter:title': 'summary',
-                'twitter:description': 'summary',
-                'twitter:site': 'summary',
-                'twitter:creator': 'summary',
-                'twitter:image': 'summary',
-                'og:title': 'summary',
-                'og:description': 'summary',
-                'og:author': 'Nicolas Ricardi',
-                'og:site_name': 'summary',
-                'og:locale': 'en_US',
-                'og:type': 'website',
-                'og:url': 'summary',
-                'og:image': 'summary'
-            },
-            favicon: '',
         }),
         new Dotenv({
             systemvars: true,
